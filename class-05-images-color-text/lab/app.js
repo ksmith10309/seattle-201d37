c83b64c7a518a466581e051375d760fff2cba5dc
Katherine Smith <katherine.lo.smith@gmail.com>
2018-07-13 15:49:22 -0700

    Solve problem 5 in lab 5

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -109,11 +109,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  var firstProduct = multiply(multArr[0], multArr[1])[0];
+  var totalProduct = multiply(firstProduct, multArr[2])[0];
 
+  var msg = 'The numbers ' + multArr.join() + ' have a product of ' + totalProduct + '.';
+  return [totalProduct, msg];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
