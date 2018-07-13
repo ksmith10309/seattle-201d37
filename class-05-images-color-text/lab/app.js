4022036ce276fa9d4b7006294e9bf638cd1da4d7
Katherine Smith <katherine.lo.smith@gmail.com>
2018-07-13 14:48:05 -0700

    Solve problem 1 in lab 5

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -10,17 +10,13 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
   var total = a + b;
-  var arr = [];
-  arr.push(total);
   var msg = 'The sum of ' + a + ' and ' + b + ' is ' + total + '.';
-  arr.push(msg);
 
-  return arr;
-  // return [a + b, 'The sum of ' + a + ' and ' + b + ' is ' + total + '.'];
+  return [total, msg];
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
